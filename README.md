e6bcbfb2120bc59a5473bb447436c54cd4d4e24c
sunilb <pavanksunil@gmail.com>
2020-09-05 21:09:47 +0530

    intila commit of resume

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# myresume
+# GraphQL Resume
+
+- Video Part 1 (Backend: Apollo Server + Nexus Schema ) https://youtu.be/_HOp7hBEjp8
+- Video Part 2 (Frontend: Apollo Client) https://youtu.be/ZFTRyuLwqdQ
+- Deployed Resume https://graphql-resume.vercel.app/
---
+++ src/data.ts
@@ -0,0 +1,79 @@
+export const data = {
+  bio: {
+    name: "Sunil Bhuvanapalli",
+    tagline: "Software Developer",
+    email: "pavanksunil@gmail.com",
+    github: "https://github.com/sunilbittu",
+    website: "https://www.google.com",
+    linkedin: "https://linkedin.com/in/sunilbhuvanapalli",
+    objective: "To build user friendly websites.",
+    skills: ["Angular", "React", "Javascript", "MongoDB", "TypeScript", "NodeJS", "SQL"],
+  },
+  
+  positions: [
+    {
+      id: "1",
+      title: "Software Engineer",
+      company: "Shyft Innovations",
+      startDate: "2020-05-01",
+      endDate: null,
+      employmentType: "FULL_TIME",
+      location: "Hyderabad, India",
+      achievements: [
+        "Development of React applications, working with Mongodb, MySQL, RESTful APIs",
+      ],
+    },
+    {
+      id: "2",
+      title: "Software Engineer",
+      company: "Cleartrip Pvt Ltd",
+      startDate: "2019-04-08",
+      endDate: "2020-04-30",
+      employmentType: "FULL_TIME",
+      location: "Bangalore, India",
+      achievements: [
+        "Building a large Angular application powering www.flyin.com + www.cleartrip.com",
+        "Working with Angular, TypeScript and Nodejs to build interactive websites.",
+        "Creating and deploying database models.",
+      ],
+    },
+    {
+      id: "3",
+      title: "Software Engineer",
+      company: "Flyin Travel and Tourism Pvt Ltd",
+      startDate: "2014-03-01",
+      endDate: "2019-04-03",
+      employmentType: "FULL_TIME",
+      location: "Hyderabad, India",
+      achievements: [
+        "Building a large Angular application for content and price management using NodeJS API",
+        "Working with Angular, TypeScript and Node to build interactive websites.",
+        "Creating and Oracle SQL procedures and tables",
+      ],
+    },
+    {
+      id: "4",
+      title: "Research Analyst",
+      company: "Progressive Digital Media",
+      startDate: "2012-02-20",
+      endDate: "2014-02-25",
+      employmentType: "FULL_TIME",
+      location: "Hyderabad, India",
+      achievements: [
+        "Developing Excel Macros and Webspiders for scrapping.",
+      ],
+    },
+    {
+      id: "5",
+      title: "Jr. Software Engineer",
+      company: "Tech Mahindra Pvt Ltc",
+      startDate: "2011-06-20",
+      endDate: "2012-01-01",
+      employmentType: "FULL_TIME",
+      location: "Hyderabad, India",
+      achievements: [
+        "Worked as a QA Engineer for API testing and Performance testing",
+      ],
+    },
+  ],
+};
